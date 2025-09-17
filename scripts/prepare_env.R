a0054031bc88e7418708f4efce9356406493e592
TWorm <tworm@ncsu.edu>
2025-09-17 09:16:47 -0400

    worldclim change

--- scripts/prepare_env.R
+++ scripts/prepare_env.R
@@ -5,10 +5,21 @@ library(terra)
 # Create data dir
 dir.create('data', showWarnings = FALSE)
 
-# Download bioclim variables at 2.5 minutes (~5km) resolution
-message('Downloading WorldClim v2 bioclim variables (resolution 2.5 minutes)')
-wc <- worldclim_global(var = 'bio', res = 2.5, path = 'data')
 
+# Create data dir
+dir.create('data', showWarnings = FALSE)
+
+message('Downloading WorldClim bioclim variables for CONUS (preferred: country-level)')
+# Try to download only USA (much smaller). If that fails, fall back to global.
+wc <- tryCatch(
+  {
+    worldclim_country(country = 'USA', var = 'bio', res = 2.5, path = 'data')
+  },
+  error = function(e) {
+    message('country download unavailable; falling back to global (may be large): ', e$message)
+    worldclim_global(var = 'bio', res = 2.5, path = 'data')
+  }
+)
 # wc is a SpatRaster stack; crop to CONUS bbox
 conus_ext <- ext(-125, -66.5, 24.5, 49.5)
 wc_conus <- crop(wc, conus_ext)
